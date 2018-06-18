ab74ed3b56e4df3c125cfb7d7d270d9544ad583d
CC <261909098@qq.com>
2018-06-18 12:53:44 +0800

    添加了错误类

---
+++ Compiler/ErrorInfo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compiler
+{
+	public class ErrorInfo
+	{
+		public int Row { get; set; }
+		public int Col { get; set; }
+
+		public ErrorType Type { get; set; }
+		public String ErrorString { get; set; }
+
+		public ErrorInfo(int row, int col, ErrorType type, String errorString)
+		{
+			Row = row;
+			Col = col;
+			Type = type;
+			ErrorString = errorString;
+		}
+		
+		// 需要生成报错信息就重写，否则就删掉吧
+		public override string ToString()
+		{
+			return base.ToString();
+		}
+
+		/// <summary>
+		/// 错误类型
+		/// </summary>
+		public enum ErrorType
+		{
+			Type1,
+			Type2,
+			Type3,
+		}
+	}
+}
