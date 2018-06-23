ac4ba5203616e0b116cf62ef9d555b39fdc74b79
MikeyQiu <scsse_qhs2015@126.com>
2018-06-24 00:26:36 +0800

    Add files via upload

--- Compiler/Node.cs
+++ Compiler/Node.cs
@@ -1,33 +1,78 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Compiler
 {
-	public class Node
+    public class Node
     {
-        public string Symbol { get; set; }        
+        public string Symbol { get; set; }//用来输出的节点内容
         public bool IsChecked { get; set; } = false;
+        public string Lexeme { get; set; } = "";//终结符的词位
+        public string value { get; set; } = "";//节点的值
+        public string syn { get; set; } = "";//节点的综合属性
+        public string inh { get; set; } = "";//节点的继承属性
+        public Env Environment { get; set; }//如果symbol是ID，那么为当前叶子节点所使用的符号表
+        public Node Parent { get; set; }
+        public List<Node> Children { get; } = new List<Node>();
+        public int row { get; set; }
+        public int col { get; set; }
 
-		public Node Parent { get; set; }
-		public List<Node> Children { get; } = new List<Node>();
+        public bool HasChild
+        {
+            get
+            {
+                return Children.Count != 0;
+            }
+        }
 
-		public bool HasChild
-		{
-			get
-			{
-				return Children.Count != 0;
-			}
-		}
+        public bool IsLeaf
+        {
+            get
+            {
+                return Lexeme != "";
+            }
+        }
 
-		public Node(string s)
+        public Node(string s)
         {
             Symbol = s;
         }
+
+        public Node(string s, string l)
+        {
+            Symbol = s;
+            Lexeme = l;
+        }
+
 
         public void Add(Node n)
         {
             Children.Add(n);
             n.Parent = this;
+        }
+
+
+        public void print(Node root)
+        {
+            Queue<Node> q = new Queue<Node>();
+            q.Enqueue(root);
+            while (q.Count != 0)
+            {
+                int count = q.Count;
+                for (int i = 0; i < count; i++)
+                {
+                    Node tmp = q.Dequeue();
+                    Console.Write(tmp.Lexeme + " ");
+                    for (int j = 0; j < tmp.Children.Count; j++)
+                    {
+                        q.Enqueue(tmp.Children[j]);
+                    }
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
---
+++ Compiler/ParserAdapter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lexical_Analyzer;
+using RE2DFA;
+
+namespace Compiler
+{
+    class ParserAdapter
+    {
+        LexicalAnalyzer la = new LexicalAnalyzer();
+        List<ErrorInfo> Errors = new List<ErrorInfo>();
+        List<LexicalData> inputData;
+        LL llgrammar;
+        Parser parser;
+
+
+        //词法分析部分，路径为程序目录下的.rule文件
+        public void LexicalAnalyze(string filepath)
+        {
+            if (!la.LoadRules(filepath))
+                return;
+            else
+                inputData = la.data;
+        }
+
+        //获取LL表，最开始就调用，路径为目录下的.txt,存放的是产生式
+        public void GenLLGrammar(string filepath)
+        {
+            llgrammar = new LL(filepath);
+        }
+
+        //语法分析部分
+        public void Parse()
+        {
+            parser = new Parser(llgrammar.nonTerminalSet, llgrammar.terminalSet, llgrammar.table, inputData);
+            parser.analyze();
+            parser.getArithValue();
+            Errors = parser.Errors;
+        }
+
+        //得到需要输出的int或者double类型变量的值
+        public List<List<string>> ArithResult()
+        {
+            if (parser.arithResult != null)
+                return parser.arithResult;
+
+            return null;
+        }
+    }
+}
