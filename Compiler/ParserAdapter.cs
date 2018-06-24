3b96975df8824ac1c91d94762fad2d0a520d38a5
huishi qiu <hsqiu@yahoo.com>
2018-06-24 14:26:30 +0800

    adapter修改

--- Compiler/ParserAdapter.cs
+++ Compiler/ParserAdapter.cs
@@ -8,32 +8,53 @@ using RE2DFA;
 
 namespace Compiler
 {
-    class ParserAdapter
+    public class ParserAdapter
     {
+        ParserResult PR = new ParserResult();
         LexicalAnalyzer la = new LexicalAnalyzer();
         List<ErrorInfo> Errors = new List<ErrorInfo>();
         List<LexicalData> inputData;
         LL llgrammar;
         Parser parser;
 
+        public async Task<ParserResult> Compile(string code, string rulePath,string CFGPath)
+        {
+            return await Task<ParserResult>.Factory.StartNew(() => 
+            {
+                GenLLGrammar(CFGPath);
+                LexicalAnalyze(rulePath,code);
+                Parse();
+                PR.Errors = Errors;
+                PR.First = llgrammar.firstMap;
+                PR.Follow = llgrammar.followMap;
+                PR.Table = llgrammar.table;
+                PR.Root = parser.root;
+                PR.ArithResult = ArithResult();
+                return PR ; 
+            });
+        }
 
         //词法分析部分，路径为程序目录下的.rule文件
-        public void LexicalAnalyze(string filepath)
+        private void LexicalAnalyze(string filepath,string input)
         {
             if (!la.LoadRules(filepath))
                 return;
             else
+            {
+                la.Analyze(input);
                 inputData = la.data;
+            }
+                
         }
 
         //获取LL表，最开始就调用，路径为目录下的.txt,存放的是产生式
-        public void GenLLGrammar(string filepath)
+        private void GenLLGrammar(string filepath)
         {
             llgrammar = new LL(filepath);
         }
 
         //语法分析部分
-        public void Parse()
+        private void Parse()
         {
             parser = new Parser(llgrammar.nonTerminalSet, llgrammar.terminalSet, llgrammar.table, inputData);
             parser.analyze();
@@ -42,12 +63,22 @@ namespace Compiler
         }
 
         //得到需要输出的int或者double类型变量的值
-        public List<List<string>> ArithResult()
+        private List<List<string>> ArithResult()
         {
             if (parser.arithResult != null)
                 return parser.arithResult;
 
             return null;
+        }
+
+        public class ParserResult
+        {
+            public List<ErrorInfo> Errors { get; set; }
+            public Dictionary<string, List<string>> First { get; set; }
+            public Dictionary<string, List<string>> Follow { get; set; }
+            public Dictionary<string, List<Dictionary<string, string>>> Table { get; set; }
+            public Node Root { get; set; }
+            public List<List<string>> ArithResult { get; set; }
         }
     }
 }
